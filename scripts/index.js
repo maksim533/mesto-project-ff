2a24000dbf70504beb2707f68b5ec1899d658480
maksim <kotyukov.maks@mail.ru>
2025-02-11 02:24:04 +0300

    готовый проект

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+
+initialCards.forEach(function (item) {
+    const cardTemplateClone = cardTemplate.querySelector('.card').cloneNode(true);
+    cardTemplateClone.querySelector('.card__image').src = item.link;
+    cardTemplateClone.querySelector('.card__title').textContent = item.name;
+    cardList.append(cardTemplateClone);
+  }
+)
+
+const buttonsDelete = document.querySelectorAll('.card__delete-button');
+buttonsDelete.forEach(function (element){
+  element.addEventListener('click', function () {
+    const card = document.querySelector('.card');
+    card.remove();
+  })
+})
